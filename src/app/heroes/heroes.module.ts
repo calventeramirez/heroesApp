9a0f530d1a116919d11f73f2817b5b91fffd7145
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-11-13 05:09:33 +0100

    Añadido la lista de superheroes y la cargad e las imagenes de estos desde local

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -8,6 +8,8 @@ import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/heroImage.pipe';
 
 
 @NgModule({
@@ -16,7 +18,9 @@ import { MaterialModule } from '../material/material.module';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
-    LayoutPageComponent
+    LayoutPageComponent,
+    CardComponent,
+    HeroImagePipe
   ],
   imports: [
     CommonModule,
---
+++ src/app/heroes/pipes/heroImage.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, type PipeTransform } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface';
+
+@Pipe({
+  name: 'heroImage',
+})
+export class HeroImagePipe implements PipeTransform {
+  transform(heroe: Hero): string {
+    if (!heroe.id && !heroe.alt_img) return 'assets/no-image.png';
+    if (heroe.alt_img) return heroe.alt_img; //Por si fuera una url
+    return `assets/heroes/${heroe.id}.jpg`;
+  }
+}
