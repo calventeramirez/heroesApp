a219d63563fbb477de9c44f7c97cea1a2334d7b5
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-11-14 04:09:19 +0100

    Añadida la funcionalidad de mas información de las tarjetas

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  public hero?: Hero;
 
+  constructor(
+    private heroesServices: HeroesService,
+    private activatedRouter: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRouter.params
+      .pipe(
+        // delay(3000),
+        switchMap(({ id }) => this.heroesServices.getHeroById(id)),
+      ).subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        return;
+      });
+  }
+
+  goBack():void{
+    this.router.navigateByUrl("heroes/list");
+  }
 }
