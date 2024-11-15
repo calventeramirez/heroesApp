3071562957b0a62d92ac624817e80cdb99913e3d
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-11-15 04:21:45 +0100

    Añadido formulario reactivo a la vista de añadir

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
-import { ValueChangeEvent } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NewPageComponent {
+  public heroeForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', {nonNullable: true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public creadores = [
-    {id: 'DC Comics', desc: "DC - Comics"},
-    {id: 'Marvel Comics', desc: "Marvel - Comics"},
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+  onSubmit():void{
+    console.log({formIsValid: this.heroeForm.valid, value: this.heroeForm.value});
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from '../../../environments/environment';
 
@@ -20,7 +20,28 @@ export class HeroesService {
       .pipe(catchError((error) => of(undefined)));
   }
 
-  getRecomendaciones(query:string):Observable<Hero[]>{
-      return this.httpClient.get<Hero[]>(this.baseUrl+'/heroes?q='+query+"&_limit=6");
+  getRecomendaciones(query: string): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(
+      this.baseUrl + '/heroes?q=' + query + '&_limit=6'
+    );
+  }
+
+  anadirHeroe(heroe: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(this.baseUrl + '/heroes', heroe);
+  }
+
+  actualizarHeroe(heroe: Hero): Observable<Hero> {
+    if (!heroe.id) throw Error('Se requiere la ID del héroe');
+    return this.httpClient.patch<Hero>(
+      this.baseUrl + '/heroes' + heroe.id,
+      heroe
+    );
+  }
+
+  borrarHeroe(id: string): Observable<boolean> {
+    return this.httpClient.delete<Hero>(this.baseUrl + '/heroes' + id).pipe(
+      catchError((error) => of(false)),
+      map((resp) => true)
+    );
   }
 }
